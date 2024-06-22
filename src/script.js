96687a47f2b114e9e53e36225f89411ce634d9c8
black-knight457 <greg.castillo05@yahoo.com.ph>
2024-06-22 09:31:31 +0000

    FINAL UPDATE

--- src/script.js
+++ src/script.js
@@ -1,17 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const display = document.getElementById('calc-display');
     const buttons = document.getElementsByClassName('btn');
 
     let currentValue = "";
-    function evaluateResult (){
+    function evaluateResult() {
         const convertedValue = currentValue
-        .replace("×", "*") 
-        .replace("÷", "/")
-        .replace('%', '*0.01')
-        .replace('sin', 'Math.sin')
-        .replace('cos', 'Math.cos')
-        .replace('ln', 'Math.log')
-        .replace('π', 'Math.PI');
+            .replace("×", "*")
+            .replace("÷", "/")
+            .replace('%', '*0.01')
+            .replace('sin', 'Math.sin')
+            .replace('cos', 'Math.cos')
+            .replace('ln', 'Math.log')
+            .replace('π', 'Math.PI')
+            .replace('log', 'Math.log10')
+            .replace('e', 'Math.E')
+            .replace('tan', 'Math.tan')
+            .replace('√', 'Math.sqrt')
+            ;
+
 
         console.log('convertedValue: ', convertedValue);
         const result = eval(convertedValue);
@@ -23,19 +29,27 @@ document.addEventListener('DOMContentLoaded', function() {
     for (let i = 0; i < buttons.length; i++) {
         const button = buttons[i];
         button.addEventListener('click', function () {
-        const value = button.innerText;
+            const value = button.innerText;
 
-        if (value == "AC"){
-            currentValue = "";
-            display.value = currentValue;
-        } else if (value == "="){
-            evaluateResult();
-        }
-        
-        else{
-        currentValue += value;
-        display.value = currentValue; 
-        }
-    })
+            try {
+                if (value == "AC") {
+                    currentValue = "";
+                    display.value = currentValue;
+                } else if (value == "=") {
+                    evaluateResult();
+                }
+
+                else {
+                    currentValue += value;
+                    display.value = currentValue;
+                }
+            }
+            catch (error) {
+                console.error(error);
+                currentValue = "ERROR";
+                display.value = currentValue;
+            }
+
+        })
     }
 });
